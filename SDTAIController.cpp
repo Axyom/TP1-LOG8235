a5d34b40fdfa381612a716cf7223f9aebcc1135c
Yolioreo <aurelien.gignon@hotmail.fr>
2019-02-08 14:16:11 -0500

    IMTHEBEST

--- SDTAIController.cpp
+++ SDTAIController.cpp
@@ -1,127 +1,191 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
-
 #include "SoftDesignTraining.h"
 #include "SDTAIController.h"
+#include "SDTCollectible.h"
+#include "SoftDesignTrainingMainCharacter.h"
 #include "Engine.h"
-#include <cstdlib>
-
-
+#include <iostream>
+#include <string>
+#include <regex>
 
 void ASDTAIController::Tick(float deltaTime)
 {
-	UWorld * World = GetWorld();
+	
 	APawn* pawn = GetPawn();
 
-	// hyperparametres
-	const float viewDistance = 80;
 
-	TArray<TEnumAsByte<EObjectTypeQuery>> TraceObjectTypes;
-	TraceObjectTypes.Add(UEngineTypes::ConvertToObjectType(ECollisionChannel::ECC_WorldStatic));
-	FHitResult losHit;
-	FCollisionQueryParams queryParams = FCollisionQueryParams();
-	queryParams.AddIgnoredActor(pawn);
-	bool collision = GetWorld()->LineTraceSingleByObjectType
-	(
-		losHit,
-		pawn->GetActorLocation(),
-		pawn->GetActorLocation() + viewDistance * pawn->GetActorForwardVector(),
-		TraceObjectTypes, queryParams
-	);
+UPROPERTY()
+	float angleRotation = 45.f;
 
-	if (collision)
-	{
-		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::White, TEXT("MUR"));
-		//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::White, TEXT(losHit.ImpactPoint));
-	}
-	else
-	{
-		FVector someMovementVector = pawn->GetActorForwardVector();
-		agentMovement(pawn, someMovementVector);
-	}
-}
 
-/**
-* Moves and orientate the agent based on the force applied + small alteration to the movements
-* so that he doesn't move strait (randomized)
-**/
-void ASDTAIController::agentMovement(APawn* pawn, FVector acceleration) {
-	UPROPERTY(EditAnywhere)
-		float maxSpeed = 0.5f;
-	UPROPERTY(EditAnywhere)
-		double randomMovementAngle = 0.005;
-	if (rand() % 2 == 1) {
-		acceleration = acceleration.RotateAngleAxis(randomMovementAngle, FVector::UpVector);
+
+	
+	
+	if (this->isRotating == true) {
+		
+		rotationInstance(pawn, angleRotation);
 	}
 	else {
-		acceleration = acceleration.RotateAngleAxis(-randomMovementAngle, FVector::UpVector);
+		stateMachine();
 	}
-	FVector newMovement = velocityCalculator(acceleration, maxSpeed, pawn->GetMovementInputVector());
+	
+}
+
+void ASDTAIController::stateMachine() {
+	UWorld * World = GetWorld();
+	APawn* pawn = GetPawn();
+	FVector hitLocation;
+
+
+
+	this->state = CheckThingsInFrontOfYou(pawn, World, this->distanceVision, hitLocation);
+
+	switch (this->state) {
+
+	case 0:
+		//RunDown
+		moveToBro(pawn, pawn->GetActorForwardVector());
+		this->state = 0;
+		break;
+	case 1: {
+		//Is there a wall or a death plate ? avoid it randomly
+
+		int rand = FMath::RandRange(0, 1);
+		if (rand == 1) {
+			this->sideRotating = -this->sideRotating;
+		}
+		this->isRotating = true;
+		this->state = 1;
+		break;
+	}
+	case 2:
+		//PickUp Detected ? go for it
+		moveToBro(pawn, hitLocation);
+		this->state = 2;
+		break;
+
+	case 3:
+		//It a player ! grab him or run!
+		moveToBro(pawn, hitLocation);
+		this->state = 3;
+		break;
+	case 4:
+		//It a player !  run!
+		moveToBro(pawn, hitLocation);
+		this->isRotating = true;
+		this->state = 3;
+		break;
+	}
+}
+void ASDTAIController::rotateBro(APawn* pawn, float angle) {
+	FRotator rot = FRotator(0, angle, 0);
+	pawn->AddActorWorldRotation(rot);
+}
+void ASDTAIController::rotationInstance(APawn* pawn, float angleRotation) {
+	float rot;
+	//Wait and decomposing rotation
+	if (this->state == 1) {
+		//Rotating because of a wall
+		rot = angleRotation;
+	}
+	if (this->state == 3) {
+		//half turn cause running from the poweredUp Player
+		rot = 180;
+
+	}
+	this->turnMovement--;
+	rotateBro(pawn, this->sideRotating*rot / 5);
+	if (this->turnMovement == 0) {
+		this->turnMovement = 5;
+		this->isRotating = false;
+	}
+}
+void ASDTAIController::moveToBro(APawn* pawn, FVector target) {
+	FVector mouvement = target;
+	if(pawn->GetActorForwardVector() != target) {
+		mouvement = target - pawn->GetActorForwardVector();
+	}
+	
+	
+	FVector newMovement = velocityCalculator(mouvement, this->maxSpeed, pawn->GetMovementInputVector());
 	pawn->AddMovementInput(newMovement, 1.f, true);
 	
-	// TODO: Fix this cancer, plz
-	// pawn->AddActorWorldRotation(newMovement.Rotation());
 }
+int ASDTAIController::CheckThingsInFrontOfYou(APawn* pawn, UWorld * world, float distanceVision,FVector& hitLocation) {
+	TArray < struct FHitResult >  OutHits;
+	TArray<TEnumAsByte<EObjectTypeQuery>> TraceObjectTypes;
+	//TraceObjectTypes.Add(UEngineTypes::ConvertToObjectType(ECollisionChannel::ECC_WorldStatic));
+	FCollisionQueryParams queryParams = FCollisionQueryParams();
+	queryParams.AddIgnoredActor(pawn);
+	
+	FCollisionShape shape = FCollisionShape();
+	shape.MakeBox(FVector(distanceVision,distanceVision,distanceVision));
+
+	//Checking for Wall
+	world->SweepMultiByObjectType(OutHits, pawn->GetActorLocation(), pawn->GetActorLocation() + distanceVision/3 * pawn->GetActorForwardVector(), FQuat::Identity, TraceObjectTypes, shape, queryParams);
+	if (OutHits.Num() != 0) {
+		//I see a Wall there
+		if (OutHits[0].GetActor()->GetHumanReadableName().Find(FString("Wall")) == 0) {
+			
+			return 1;
+		}
+	}
+	
+	//Checking For player or Collectible
+	world->SweepMultiByObjectType(OutHits, pawn->GetActorLocation(), pawn->GetActorLocation() + distanceVision * pawn->GetActorForwardVector(), FQuat(ForceInitToZero), TraceObjectTypes, shape, queryParams);
+	if (OutHits.Num() != 0) {
+		if (ASoftDesignTrainingMainCharacter* mainActor = Cast<ASoftDesignTrainingMainCharacter>(OutHits[0].GetActor())) {
+
+			//i see a human there
+			//Check if he is powered Up, in this cas run away
+			if (mainActor->IsPoweredUp()) {
+				GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::White, TEXT("RUN"));
+				hitLocation = -pawn->GetActorForwardVector();
+				return 4;
+			}
+			else {
+				hitLocation = pawn->GetActorForwardVector();
+				return 3;
+			}
+			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::White, hitLocation.ToCompactString());
+
+		}
+
+		if (ASDTCollectible* collectibleActor = Cast<ASDTCollectible>(OutHits[0].GetActor())) {
+			//I see a collectible there
+			//Is it visible
+			if (!collectibleActor->IsOnCooldown()) {
+				//GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::White, TEXT("Jevoisuncollectible"));
+				hitLocation = OutHits[0].GetActor()->GetActorLocation();
+				return 2;
+			}
+			
+		}
+		
+	}
+	//Checking For DeathFloor so I look at the floor in front of the player
+	TArray < struct FHitResult > floor;
+	TraceObjectTypes.Add(UEngineTypes::ConvertToObjectType(ECollisionChannel::ECC_GameTraceChannel3));
+	world->LineTraceMultiByObjectType(floor, pawn->GetActorLocation(), pawn->GetActorLocation() + distanceVision / 2  * pawn->GetActorForwardVector() + FVector(0, 0, -100.f), TraceObjectTypes, queryParams);
+	if (floor.Num() != 0) {
+		if (floor[0].GetActor()->GetHumanReadableName().Find(FString("BP_Death")) == 0) {
+			//We identify a DeathFloor in front of the Pawn
+			return 1;
+		}
+	}
+
+	hitLocation = pawn->GetActorForwardVector();
+	return 0;
+}
+
 
 FVector ASDTAIController::velocityCalculator(FVector acceleration, float maxSpeed, FVector currentVelocity) {
 	FVector newVelocity = currentVelocity + acceleration;
 	if (newVelocity.Size() > maxSpeed) {
 		newVelocity.Normalize();
 		newVelocity = newVelocity * maxSpeed;
-	}
+	}	
 	return newVelocity;
 }
 
-/*void default_behaviour()
-{
-	if ()
-	{
-		physicHelper.CastRay(selfPosition, targetActor->GetActorLocation(), hitResult, true);
-	}
-	else if ()
-	{
-	}
-	else
-	{
-		pawn->AddMovementInput(pawn->GetActorForwardVector(), 1., true);
-	}
-}*/
-
-
-
-/*bool ASDTAIController::MoveToTarget(FVector2D target, float targetSpeed, float deltaTime)
-{
-APawn* pawn = GetPawn();
-
-FVector const pawnPosition(pawn->GetActorLocation());
-
-FVector2D const toTarget = target - FVector2D(pawnPosition);
-
-FVector2D const displacement = FMath::Min(toTarget.Size(), FMath::Min(m_maxSpeed, targetSpeed) * deltaTime) * toTarget.GetSafeNormal();
-
-pawn->SetActorLocation(pawnPosition + FVector(displacement, 0.f), true);
-
-pawn->SetActorRotation(FVector(displacement, 0.f).ToOrientationQuat());
-
-return toTarget.Size() < FMath::Min(m_maxSpeed, targetSpeed) * deltaTime;
-
-}
-
-
-
-void ASDTAIController::MoveTowardsDirection(FVector2D direction, float targetSpeed, float deltaTime)
-
-{
-
-APawn* pawn = GetPawn();
-
-FVector const pawnPosition(pawn->GetActorLocation());
-
-FVector2D const displacement = FMath::Min(m_maxSpeed, targetSpeed) * deltaTime * direction.GetSafeNormal();
-
-pawn->SetActorLocation(pawnPosition + FVector(displacement, 0.f), true);
-
-pawn->SetActorRotation(FVector(direction, 0.f).ToOrientationQuat());
-
-}*/
--- SDTAIController.h
+++ SDTAIController.h
@@ -1,36 +1,34 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
-
 #pragma once
-
-
 
 #include "CoreMinimal.h"
 #include "AIController.h"
+
 #include "SDTAIController.generated.h"
 
-
-
 /**
-*
-*/
-
+ * 
+ */
 UCLASS(ClassGroup = AI, config = Game)
-
 class SOFTDESIGNTRAINING_API ASDTAIController : public AAIController
 {
-
-	GENERATED_BODY()
-
+    GENERATED_BODY()
 public:
-
-	virtual void Tick(float deltaTime) override;
-	void agentMovement(APawn* pawn, FVector force);
+    virtual void Tick(float deltaTime) override;
 	FVector velocityCalculator(FVector acceleration, float maxSpeed, FVector currentVelocity);
-
+	int CheckThingsInFrontOfYou(APawn* pawn, UWorld * World, float distanceVision, FVector& hitLocation);
+	void rotateBro(APawn* pawn, float angle);
+	void rotationInstance(APawn* pawn, float angleRotation);
+	void moveToBro(APawn* pawn, FVector target);
+	void stateMachine();
 private:
-
-	enum State_t { TURN, RUNAWAY } m_state;
-
+	int turnMovement = 5;
+	bool isRotating = false;
+	int sideRotating = 1;
+	int state = 0;
+	UPROPERTY(EditAnywhere)
+	float maxSpeed = 1.5f;
+	UPROPERTY(EditAnywhere)
+	float distanceVision = 500.f;
 };
